10073fc89d118a643942fa2713aff5c1edad828d
NjengaBen <bennjenga2@gmail.com>
2024-08-01 23:01:21 +0300

    drag

--- app/page.js
+++ app/page.js
@@ -44,15 +44,52 @@ export default function Home() {
         <motion.div
           variants={gridSquareVariants}
           className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"
-        ></motion.div>
+        >
+          <motion.div
+            className="w-1/3 h-1/3 shadow-md bg-rose-400"
+            animate={{
+              scale: [1, 2, 2, 1],
+              rotate: [0, 90, 90, 0],
+              borderRadius: ["10%", "10%", "50%", "10%"],
+            }}
+            transition={{
+              duration: 5,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatDelay: 1,
+            }}
+          />
+        </motion.div>
+        {/* Hover and Tap */}
         <motion.div
           variants={gridSquareVariants}
           className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"
-        ></motion.div>
+        >
+          <motion.button
+            className="w-1/2 bg-emerald-600 py-4 rounded-lg text-2xl text-gray-100 font-light tracking-wide"
+            whileTap={{ scale: 0.9 }}
+            whileHover={{
+              scale: 1.1,
+              backgroundColor: "#d1d5db",
+              color: "black",
+            }}
+            transition={{ bounceDamping: 10, bounceStiffness: 600 }}
+          >
+            Subscribe
+          </motion.button>
+        </motion.div>
+        {/* Drag movements */}
         <motion.div
           variants={gridSquareVariants}
           className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"
-        ></motion.div>
+        >
+          <motion.div
+            className="w-1/3 h-1/3 bg-orange-500 rounded-lg cursor-grab"
+            drag
+            dragConstraints={{ top: -125, right: 125, bottom: 125, left: -125 }}
+            dragTransition={{ bounceDamping: 10, bounceStiffness: 600 }}
+          />
+        </motion.div>
         <motion.div
           variants={gridSquareVariants}
           className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"
